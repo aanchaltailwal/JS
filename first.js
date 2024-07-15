858e679fca2d5ed891324270bfb80666856b074c
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-07-15 20:22:47 +0530

    objects

--- first.js
+++ first.js
@@ -1,2 +1,15 @@
 console.log("Welcome Aanchal");
 console.log("I love javaScript");
+
+//variables
+fullname ="Aanchal";
+x= null;
+isFollow = true;  //boolean
+console.log(isFollow);
+
+const student = {
+    fullName : "Aanchal Tailwal",
+    age : 20,
+    cgpa : 8.48,
+    isPass : true,
+}
---
+++ object.js
@@ -0,0 +1,7 @@
+const student = {
+    fullName : "Aanchal Tailwal",
+    age : 20,
+    cgpa : 8.48,
+    isPass : true,
+}
+console.log(student);
