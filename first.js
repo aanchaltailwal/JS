ecf6b511a9279c4295b24a1e776414b7d1572b2d
Aanchal Tailwal <tailwalaanchal@gmail.com>
2024-07-16 14:24:29 +0530

    ques3

--- first.js
+++ first.js
@@ -42,14 +42,23 @@ else{
 console.log(color);
 
 //alert
-alert("hello")            //creates one time pop up
+//alert("hello")            //creates one time pop up
 
 //
-let n = prompt("Enter a number");             //it can take input from user
+let n = prompt("Enter your Marks:");             //it can take input from user
 
-if(n % 5 == 0){
-    console.log("Multiple of 5", n);
+if(n >= 90 && n <= 100){
+    console.log("A");
 
+}else if(n >= 70  && n <= 89 ){
+    console.log("B");
+}else if(n >= 60 && n <= 69 ){
+    console.log("C");
+}else if(n >= 50  && n <= 59 ){
+    console.log("B");
 }else{
-    console.log("Not a multiple of 5", n);
+    console.log("F");
 }
+  
+  
+    
